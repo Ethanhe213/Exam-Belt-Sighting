a0f3a1cf3c8dba3e247de73c95412e5de084d711
Ethan He <ethanhe.213@gmail.com>
2022-12-18 21:31:41 -0500

    fix casing

--- flask_app/models/event.py
+++ flask_app/models/event.py
@@ -2,7 +2,7 @@ from flask_app.config.mysqlconnection import connectToMySQL
 from flask import flash,session
 from flask_app.models import user
 
-DB='websighting'
+DB='Websighting'
 class Event:
     def __init__(self,data):
         self.id=data['id']
